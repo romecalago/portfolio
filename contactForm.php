1464eec7216c9aca20db3d372f9c2db2a4e069e0
Rome <romecalago@gmail.com>
2021-01-12 16:11:05 +0800

    update

---
+++ contactForm.php
@@ -0,0 +1,19 @@
+<?php
+
+    if(isset($_POST['submit'])) {
+        $name = $_POST['name'];
+        $emailFrom = $_POST['email'];
+        $subject = $_POST['subject'];
+        $message = $_POST['message'];
+
+        $mailTO = "romecalago@gmail.com";
+        // $mailTO = "fovajox829@izzum.com";
+        $headers = "From: " . $emailFrom;
+        $txt = "You have an email from " . $name . "\n\n" . $message;
+
+        mail($mailTO, $subject, $txt, $headers);
+        header("Location: index.php?mailsend")
+
+    }
+
+?>
