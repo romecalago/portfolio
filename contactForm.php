9e5ee6320b6eaf1ff078619c57e726eafce5452c
Rome <romecalago@gmail.com>
2021-01-12 20:28:46 +0800

    update

--- contactForm.php
+++ contactForm.php
@@ -1,19 +1,44 @@
 <?php
 
-    if(isset($_POST['submit'])) {
-        $name = $_POST['name'];
-        $emailFrom = $_POST['email'];
-        $subject = $_POST['subject'];
-        $message = $_POST['message'];
+use PHPMailer\PHPMailer\PHPMailer;
 
-        // $mailTO = "romecalago@gmail.com";
-        $mailTO = "dark_raven047@yahoo.com.ph";
-        $headers = "From: " . $emailFrom;
-        $txt = "You have an email from " . $name . "\n\n" . $message;
+if(isset($_POST['name']) && isset($_POST['email'])) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $body = $_POST['body'];
 
-        mail($mailTO, $subject, $txt, $headers);
-        header("Location: index.php?mailsend");
+    require_once "PHPMailer/PHPMailer.php";
+    require_once "PHPMailer/SMTP.php";
+    require_once "PHPMailer/Exception.php";
 
+    $mail = new PHPMailer();
+
+    //SMTP settings
+    $mail ->isSMTP();
+    $mail ->Host = "smtp.gmail.com";
+    $mail ->SMTPAuth = true;
+    $mail ->Username = "cobitX19@gmail.com";
+    $mail ->Password = "iladakoTITS!09";
+    $mail ->Port = 456;
+    $mail ->SMTPSecure = "ssl";
+
+    //email settings
+    $mail ->isHTML(true);
+    $mail ->setFrom($email, $name);
+    $mail ->addAddress("cobitx19@gmail.com");
+    $mail ->Subject($email ($subject));
+    $mail ->Body = $body
+
+    if ($mail->send()) {
+        $status = "success";
+        $response = "Email is sent!";
+    } else {
+        $status = "failed";
+        $response = "Something is wrong: <br>" . $mail->ErrorInfo;
     }
+    exit(json_encode(array("status" => $status, "response" => $response)));
+
+}
 
 ?>
